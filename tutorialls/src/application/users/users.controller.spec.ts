3027bf7485ed1e3433e1c9fa2dca6727441d0f3d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 19:46:56 -0300

    [ :white_check_mark: ] | create: test - controller [user] (module::application)

--- tutorialls/src/application/users/users.controller.spec.ts
+++ tutorialls/src/application/users/users.controller.spec.ts
@@ -1,20 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
-import { AuthService } from './auth.service';
+import { MODULE } from 'src/app.registry';
+import { IAuthService } from 'src/domain/service/user/auth.service';
+import { ISignupUserDTO } from 'src/domain/DTO/user/register.dto';
+import { IAuthUserDTO } from 'src/domain/DTO/user/auth.dto';
+import { User } from 'src/domain/entity/user.entity';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let authServiceMock: jest.Mocked<IAuthService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: MODULE.USER.SERVICE.AUTH,
+          useValue: {
+            signup: jest.fn(),
+            login: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    authServiceMock = module.get(MODULE.USER.SERVICE.AUTH);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  describe('signup', () => {
+    it('should call AuthService.signup with the correct user data', async () => {
+      const userMock: ISignupUserDTO = {
+        email: 'test@test.com',
+        password: '12345',
+      };
+      const userEntityMock = {} as User;
+
+      authServiceMock.signup.mockResolvedValueOnce(userEntityMock);
+
+      const result = await usersController.signup(userMock);
+
+      expect(authServiceMock.signup).toHaveBeenCalledWith(userMock);
+      expect(result).toBe(userEntityMock);
+    });
+  });
+
+  describe('login', () => {
+    it('should call AuthService.login with the correct user data', async () => {
+      const userMock: IAuthUserDTO = {
+        email: 'test@test.com',
+        password: '12345',
+      };
+      const userEntityMock = {} as User;
+
+      authServiceMock.login.mockResolvedValueOnce(userEntityMock);
+
+      const result = await usersController.login(userMock);
+
+      expect(authServiceMock.login).toHaveBeenCalledWith(userMock);
+      expect(result).toBe(userEntityMock);
+    });
   });
 });
