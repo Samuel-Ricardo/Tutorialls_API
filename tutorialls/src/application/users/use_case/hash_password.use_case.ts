f2fa37a24472b8c2805dd334e6bf1d375f7b0db7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 17:30:55 -0300

    [ :lock_with_ink_pen: ] | create: engine - hashing > bcrypt (module::infra)

--- tutorialls/src/application/users/use_case/hash_password.use_case.ts
+++ tutorialls/src/application/users/use_case/hash_password.use_case.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { IHashPasswordDTO } from 'src/domain/DTO/user/hash/password.dto';
 import { IHashPasswordUseCase } from 'src/domain/use_case/user/security/hash_password.use_case';
-import * as bcrypt from 'bcrypt';
+import { bcrypt } from 'src/infra/engine/hashing/bcrypt.engine';
 
 @Injectable()
 export class BcryptHashPasswordUseCase implements IHashPasswordUseCase {
