25027d957afbe4be192342b5b19c2888910eb9c8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 18:18:29 -0300

    [ :card_file_box: ] | implements: repository - user > signup (module::application)

---
+++ tutorialls/src/application/users/repository/prisma/user.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { IFindUserByEmailDTO } from 'src/domain/DTO/user/find/by/email.dto';
+import { IFindUserByIdDTO } from 'src/domain/DTO/user/find/by/id.dto';
+import { ISignupUserDTO } from 'src/domain/DTO/user/register.dto';
+import { IUserDTO } from 'src/domain/DTO/user/user.dto';
+import { User } from 'src/domain/entity/user.entity';
+import { IUserRepository } from 'src/domain/repository/user/user.repository';
+import { PrismaService } from 'src/infra/engine/database/prisma/prisma.service';
+
+@Injectable()
+export class PrismaUserRepository implements IUserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async signup(user: ISignupUserDTO): Promise<User> {
+    return User.fromDTO(
+      this.prisma.user.create({ data: user }) as unknown as IUserDTO,
+    );
+  }
+
+  findByEmail(DTO: IFindUserByEmailDTO): Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+  findById(DTO: IFindUserByIdDTO): Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+}
