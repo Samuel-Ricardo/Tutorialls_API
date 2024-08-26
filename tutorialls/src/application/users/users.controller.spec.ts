1635b16c5aae9ca818493cb340326280d9f21b6c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 18:10:16 -0300

    [ :recycle: ] | rename: service - user ->auth (module::application)

--- tutorialls/src/application/users/users.controller.spec.ts
+++ tutorialls/src/application/users/users.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { AuthService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +8,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [AuthService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
