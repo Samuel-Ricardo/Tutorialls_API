d5a128a8d30fcea2826c36808765aa61a3e7ad72
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 18:22:39 -0300

    [ :syringe: ] | update: module - user [repository] (module::application)

--- tutorialls/src/application/users/users.module.ts
+++ tutorialls/src/application/users/users.module.ts
@@ -4,10 +4,12 @@ import { UsersController } from './users.controller';
 import { UserShouldNotAlreadyExistsToSignupPolicy } from './policy/alredy_exists.policy';
 import { BcryptHashPasswordUseCase } from './use_case/hash_password.use_case';
 import { RepositorySignupUserUseCase } from './use_case/signup.use_case';
+import { PrismaUserRepository } from './repository/prisma/user.repository';
 
 @Module({
   controllers: [UsersController],
   providers: [
+    PrismaUserRepository,
     AuthService,
     UserShouldNotAlreadyExistsToSignupPolicy,
     BcryptHashPasswordUseCase,
