93108dabf064ad4c0e4f3d0fd52d06d81909ea44
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 17:31:18 -0300

    [ :recycle: ] | rename: policy - user alredy exists (module::domain)

--- tutorialls/src/application/users/policy/alredy_exists.policy.ts
+++ tutorialls/src/application/users/policy/alredy_exists.policy.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { IUserAlreadyExistsPolicyDTO } from 'src/domain/DTO/user/policy/alredy_exists.dto';
-import { IUserAlreadyExistsPolicy } from 'src/domain/policy/user/alredy_exists.policy';
+import { IUserShouldNotAlreadyExistsToSignupPolicy } from 'src/domain/policy/user/alredy_exists.policy';
 import { IUserRepository } from 'src/domain/repository/user/user.repository';
 
 @Injectable()
-export class UserAlredyExistsPolicy implements IUserAlreadyExistsPolicy {
+export class UserShouldNotAlreadyExistsToSignupPolicy
+  implements IUserShouldNotAlreadyExistsToSignupPolicy
+{
   constructor(private readonly repository: IUserRepository) {}
 
   async execute({ email }: IUserAlreadyExistsPolicyDTO) {
