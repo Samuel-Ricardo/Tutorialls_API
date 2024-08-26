99083ce8248c552692b6024ca26b94fb8ba8e96d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 19:45:23 -0300

    [ :syringe: ] | update: module - user (module::application)

--- tutorialls/src/app.module.ts
+++ tutorialls/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './application/users/users.module';
 import { PrismaModule } from './infra/engine/database/prisma/prisma.module';
-import { Module } from './application/user/.module';
-import { UsersModule } from './application/users/users/users.module';
 
 @Module({
-  imports: [PrismaModule, Module, UsersModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
