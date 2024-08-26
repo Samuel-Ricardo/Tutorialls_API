51eb18242f1c036c6ee1a91dee8d648c7e8360a8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 16:02:04 -0300

    [ :recycle: ] | rename: policy - user alredy exists (module::domain)

--- tutorialls/src/domain/policy/user/alredy_exists.policy.ts
+++ tutorialls/src/domain/policy/user/alredy_exists.policy.ts
@@ -1,5 +1,5 @@
 import { IUserAlreadyExistsPolicyDTO } from 'src/domain/DTO/user/policy/alredy_exists.dto';
 
-export interface IUserAlreadyExistsPolicy {
-  exexecute(user: IUserAlreadyExistsPolicyDTO): Promise<boolean>;
+export interface IUserShouldNotAlreadyExistsToSignupPolicy {
+  execute(user: IUserAlreadyExistsPolicyDTO): Promise<boolean>;
 }
