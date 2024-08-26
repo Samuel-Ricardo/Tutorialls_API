f3391ab3115784cd66f46d355b778f25334ecd7e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 17:20:47 -0300

    [ :passport_control: ] | implements: use-case - hash > password [user] (module::domain)

---
+++ tutorialls/src/application/users/use_case/hash_password.use_case.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { IHashPasswordDTO } from 'src/domain/DTO/user/hash/password.dto';
+import { IHashPasswordUseCase } from 'src/domain/use_case/user/security/hash_password.use_case';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptHashPasswordUseCase implements IHashPasswordUseCase {
+  execute({ password }: IHashPasswordDTO): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+}
--- tutorialls/src/application/users/users.module.ts
+++ tutorialls/src/application/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserAlredyExistsPolicy } from './policy/alredy_exists.policy';
+import { BcryptHashPasswordUseCase } from './use_case/hash_password.use_case';
 
 @Module({
   controllers: [UsersController],
-  providers: [AuthService, UserAlredyExistsPolicy],
+  providers: [AuthService, UserAlredyExistsPolicy, BcryptHashPasswordUseCase],
 })
 export class UsersModule {}
