2127325923a5bac5937d3d4f3027428943ebfb7e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 20:45:24 -0300

    [ :white_check_mark: ] | update: test - controller > user (module::application)

--- tutorialls/src/application/users/users.controller.unit.spec.ts
+++ tutorialls/src/application/users/users.controller.unit.spec.ts
@@ -1,16 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { MODULE } from 'src/app.registry';
 import { IUserService } from 'src/domain/service/user/user.service';
 import { ISignupUserDTO } from 'src/domain/DTO/user/register.dto';
 import { ILoginUserDTO } from 'src/domain/DTO/user/login.dto';
+import { DecryptUserPipe } from './pipe/encryption/encryption.pipe';
+import { ZodValidationPipe } from './pipe/validation/zod/zod.pipe';
+import { EncryptionModule } from '../encryption/encryption.module';
 
 describe('UsersController (Unit)', () => {
   let usersController: UsersController;
   let userServiceMock: jest.Mocked<IUserService>;
 
+  let decryptUserPipeMock: jest.Mocked<DecryptUserPipe>;
+  let zodValidationPipeMock: jest.Mocked<ZodValidationPipe>;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [EncryptionModule],
       controllers: [UsersController],
       providers: [
         {
@@ -20,11 +28,25 @@ describe('UsersController (Unit)', () => {
             login: jest.fn(),
           },
         },
+        {
+          provide: DecryptUserPipe,
+          useValue: {
+            transform: jest.fn().mockImplementation((value) => value), // Mock implementation
+          },
+        },
+        {
+          provide: ZodValidationPipe,
+          useValue: {
+            transform: jest.fn().mockImplementation((value) => value), // Mock implementation
+          },
+        },
       ],
     }).compile();
 
     usersController = module.get<UsersController>(UsersController);
     userServiceMock = module.get(MODULE.USER.SERVICE.AUTH);
+    decryptUserPipeMock = module.get(DecryptUserPipe);
+    zodValidationPipeMock = module.get(ZodValidationPipe);
   });
 
   it('should be defined', () => {
