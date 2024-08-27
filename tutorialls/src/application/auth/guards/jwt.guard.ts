b51ef3b2253eac6f08020f8ef16ddf0082a70abc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 12:41:48 -0300

    [ :passport_control: ] | create: guard - auth [jwt] (module::application)

---
+++ tutorialls/src/application/auth/guards/jwt.guard.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    Logger.log('Intercepted By Guard: JWT Auth');
+    return super.canActivate(context);
+  }
+}
