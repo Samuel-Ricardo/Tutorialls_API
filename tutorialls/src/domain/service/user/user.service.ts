68a86f43b4fe6d6f6e106871a19605f1330fe165
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 12:36:10 -0300

    [ :recycle: ] | refact: service - auth (module::domain)

--- tutorialls/src/domain/service/user/user.service.ts
+++ tutorialls/src/domain/service/user/user.service.ts
@@ -1,8 +1,8 @@
+import { ILoginOutputDTO } from 'src/domain/DTO/output/user/login.dto';
 import { ILoginUserDTO } from 'src/domain/DTO/user/login.dto';
 import { ISignupUserDTO } from 'src/domain/DTO/user/register.dto';
-import { User } from 'src/domain/entity/user.entity';
 
 export interface IUserService {
-  login(user: ILoginUserDTO): Promise<User>;
-  signup(user: ISignupUserDTO): Promise<User>;
+  login(user: ILoginUserDTO): Promise<ILoginOutputDTO>;
+  signup(user: ISignupUserDTO): Promise<void>;
 }
