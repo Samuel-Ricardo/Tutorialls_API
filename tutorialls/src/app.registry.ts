228e43cc1ff8d25ddd0df3ac4b454f98276d5d11
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 10:08:41 -0300

    [ :syringe: ] | create: registry - auth (module::application)

--- tutorialls/src/app.registry.ts
+++ tutorialls/src/app.registry.ts
@@ -1,5 +1,7 @@
+import { AUTH_REGISTRY } from './application/auth/auth.regsitry';
 import { USER_REGISTRY } from './application/users/user.registry';
 
 export const MODULE = {
   USER: USER_REGISTRY,
+  AUHT: AUTH_REGISTRY,
 };
---
+++ tutorialls/src/application/auth/auth.regsitry.ts
@@ -0,0 +1,5 @@
+import { AUTH_USE_CASE_REGISTRY } from './use_case/auth.registry';
+
+export const AUTH_REGISTRY = {
+  USE_CASE: AUTH_USE_CASE_REGISTRY,
+};
