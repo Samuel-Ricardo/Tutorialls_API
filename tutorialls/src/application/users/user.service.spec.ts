b0742458a8971f325186bc9130a1adb59f7d1cab
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 12:39:51 -0300

    [ :white_check_mark: ] | create: test - controller [unit] (module::application::user)

--- tutorialls/src/application/users/user.service.spec.ts
+++ tutorialls/src/application/users/user.service.spec.ts
@@ -1,73 +1,87 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { MODULE } from 'src/app.registry';
-import { IUserShouldNotAlreadyExistsToSignupPolicy } from 'src/domain/policy/user/signup_alredy_exists.policy';
-import { IHashPasswordUseCase } from 'src/domain/use_case/user/security/hash_password.use_case';
-import { ISignupUserUseCase } from 'src/domain/use_case/user/signup.use_case';
-import { UserAlredyExistsError } from 'src/internal/lib/error/user/alredy_exists.error';
-import { ISignupUserDTO } from 'src/domain/DTO/user/register.dto';
-import { User } from 'src/domain/entity/user.entity';
+import { IUserShouldExistsToAuthPolicy } from 'src/domain/policy/user/login_alredy_exists.policy';
 import { IPasswordShouldBeValidToLoginPolicy } from 'src/domain/policy/user/password_is_valid.policy';
+import { IUserShouldNotAlreadyExistsToSignupPolicy } from 'src/domain/policy/user/signup_alredy_exists.policy';
+import { IAuthService } from 'src/domain/service/auth/auth.service';
+import { ISignupUserUseCase } from 'src/domain/use_case/user/signup.use_case';
+import { IHashPasswordUseCase } from 'src/domain/use_case/user/security/hash_password.use_case';
+import { ISignupUserDTO } from 'src/domain/DTO/user/register.dto';
+import { ILoginUserDTO } from 'src/domain/DTO/user/login.dto';
+import { UserAlredyExistsError } from 'src/internal/lib/error/user/alredy_exists.error';
+import { InvalidCredentials } from 'src/internal/lib/error/user/invalid_password.error';
+import { UserNotFoundError } from 'src/internal/lib/error/user/not_found.error';
+import { User } from 'src/domain/entity/user.entity';
 
 describe('UserService', () => {
-  let authService: UserService;
-  let hashPasswordMock: jest.Mocked<IHashPasswordUseCase>;
+  let userService: UserService;
   let userShouldNotAlreadyExistsToSignupMock: jest.Mocked<IUserShouldNotAlreadyExistsToSignupPolicy>;
-  let signupUserMock: jest.Mocked<ISignupUserUseCase>;
+  let userShouldExistsToAuthMock: jest.Mocked<IUserShouldExistsToAuthPolicy>;
   let passwordIsValidMock: jest.Mocked<IPasswordShouldBeValidToLoginPolicy>;
-  let userShouldExistsToAuthMock: jest.Mocked<IUserShouldNotAlreadyExistsToSignupPolicy>;
+  let hashPasswordMock: jest.Mocked<IHashPasswordUseCase>;
+  let signupUserMock: jest.Mocked<ISignupUserUseCase>;
+  let authServiceMock: jest.Mocked<IAuthService>;
 
   beforeEach(async () => {
+    userShouldNotAlreadyExistsToSignupMock = {
+      execute: jest.fn(),
+    } as unknown as jest.Mocked<IUserShouldNotAlreadyExistsToSignupPolicy>;
+    userShouldExistsToAuthMock = {
+      execute: jest.fn(),
+    } as unknown as jest.Mocked<IUserShouldExistsToAuthPolicy>;
+    passwordIsValidMock = {
+      execute: jest.fn(),
+    } as unknown as jest.Mocked<IPasswordShouldBeValidToLoginPolicy>;
+    hashPasswordMock = {
+      execute: jest.fn(),
+    } as unknown as jest.Mocked<IHashPasswordUseCase>;
+    signupUserMock = {
+      execute: jest.fn(),
+    } as unknown as jest.Mocked<ISignupUserUseCase>;
+    authServiceMock = {
+      authenticate: jest.fn(),
+    } as unknown as jest.Mocked<IAuthService>;
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [],
       providers: [
-        UserService,
         {
-          provide: MODULE.USER.USE_CASE.HASH.PASSWORD,
-          useValue: {
-            execute: jest.fn(),
-          },
+          provide: MODULE.USER.SERVICE.AUTH,
+          useClass: UserService,
         },
         {
           provide: MODULE.USER.POLICY.ALREDY_EXISTS,
-          useValue: {
-            execute: jest.fn(),
-          },
+          useValue: userShouldNotAlreadyExistsToSignupMock,
         },
         {
           provide: MODULE.USER.POLICY.SHOULD_EXISTS,
-          useValue: {
-            execute: jest.fn(),
-          },
+          useValue: userShouldExistsToAuthMock,
         },
         {
           provide: MODULE.USER.POLICY.IS_VALID_PASSWORD,
-          useValue: {
-            execute: jest.fn(),
-          },
+          useValue: passwordIsValidMock,
+        },
+        {
+          provide: MODULE.USER.USE_CASE.HASH.PASSWORD,
+          useValue: hashPasswordMock,
         },
         {
           provide: MODULE.USER.USE_CASE.SIGNUP,
-          useValue: {
-            execute: jest.fn(),
-          },
+          useValue: signupUserMock,
+        },
+        {
+          provide: MODULE.AUTH.SERVICE.JWT,
+          useValue: authServiceMock,
         },
       ],
     }).compile();
 
-    authService = module.get<UserService>(UserService);
-    hashPasswordMock = module.get(MODULE.USER.USE_CASE.HASH.PASSWORD);
-    userShouldNotAlreadyExistsToSignupMock = module.get(
-      MODULE.USER.POLICY.ALREDY_EXISTS,
-    );
-    signupUserMock = module.get(MODULE.USER.USE_CASE.SIGNUP);
-    passwordIsValidMock = module.get(MODULE.USER.POLICY.IS_VALID_PASSWORD);
-    userShouldExistsToAuthMock = module.get(MODULE.USER.POLICY.SHOULD_EXISTS);
-
-    console.log({ passwordIsValidMock, userShouldExistsToAuthMock });
+    userService = module.get<UserService>(MODULE.USER.SERVICE.AUTH);
   });
 
   it('should be defined', () => {
-    expect(authService).toBeDefined();
+    expect(userService).toBeDefined();
   });
 
   describe('signup', () => {
@@ -81,7 +95,7 @@ describe('UserService', () => {
         true,
       );
 
-      await expect(authService.signup(userMock)).rejects.toThrow(
+      await expect(userService.signup(userMock)).rejects.toThrow(
         UserAlredyExistsError,
       );
       expect(
@@ -100,9 +114,8 @@ describe('UserService', () => {
         false,
       );
       hashPasswordMock.execute.mockResolvedValueOnce(hashedPassword);
-      signupUserMock.execute.mockResolvedValueOnce({} as User);
 
-      const result = await authService.signup(userMock);
+      await userService.signup(userMock);
 
       expect(
         userShouldNotAlreadyExistsToSignupMock.execute,
@@ -112,7 +125,68 @@ describe('UserService', () => {
         ...userMock,
         password: hashedPassword,
       });
-      expect(result).toEqual({});
+    });
+  });
+
+  describe('login', () => {
+    it('should throw UserNotFoundError if user does not exist', async () => {
+      const userMock: ILoginUserDTO = {
+        email: 'test@test.com',
+        password: '12345',
+      };
+
+      userShouldExistsToAuthMock.execute.mockResolvedValueOnce(null);
+
+      await expect(userService.login(userMock)).rejects.toThrow(
+        UserNotFoundError,
+      );
+      expect(userShouldExistsToAuthMock.execute).toHaveBeenCalledWith(userMock);
+    });
+
+    it('should throw InvalidCredentials if password is invalid', async () => {
+      const userMock: ILoginUserDTO = {
+        email: 'test@test.com',
+        password: '12345',
+      };
+      const userEntityMock = User.fromDTO({
+        id: '1',
+        email: 'test@test.com',
+        password: 'hashedPassword',
+      });
+
+      userShouldExistsToAuthMock.execute.mockResolvedValueOnce(userEntityMock);
+      passwordIsValidMock.execute.mockResolvedValueOnce(false);
+
+      await expect(userService.login(userMock)).rejects.toThrow(
+        InvalidCredentials,
+      );
+      expect(userShouldExistsToAuthMock.execute).toHaveBeenCalledWith(userMock);
+      expect(passwordIsValidMock.execute).toHaveBeenCalledWith({
+        login_password: userMock.password,
+        registered_password: userEntityMock.toDTO().password,
+      });
+    });
+
+    it('should return a token after successful login', async () => {
+      const userMock: ILoginUserDTO = {
+        email: 'test@test.com',
+        password: '12345',
+      };
+      const userEntityMock = User.fromDTO({
+        id: '1',
+        email: 'test@test.com',
+        password: 'hashedPassword',
+      });
+      const tokenMock = 'authToken';
+
+      userShouldExistsToAuthMock.execute.mockResolvedValueOnce(userEntityMock);
+      passwordIsValidMock.execute.mockResolvedValueOnce(true);
+      authServiceMock.authenticate.mockResolvedValueOnce(tokenMock);
+
+      const result = await userService.login(userMock);
+
+      expect(result).toStrictEqual({ token: tokenMock });
+      expect(authServiceMock.authenticate).toHaveBeenCalledWith(userMock);
     });
   });
 });
