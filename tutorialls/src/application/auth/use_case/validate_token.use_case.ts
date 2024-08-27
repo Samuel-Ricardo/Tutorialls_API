638daff73592eeada6acf611d9f4840ffbeb7bb1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 10:07:43 -0300

    [ :passport_control: ] | implements: use-case - validate > token [auth] (module::application)

---
+++ tutorialls/src/application/auth/use_case/validate_token.use_case.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { IValidateAuthTokenDTO } from 'src/domain/DTO/auth/token/validate.dto';
+import { IValidateAuthTokenUseCase } from 'src/domain/use_case/auth/validate_token.use_case';
+
+@Injectable()
+export class ValidateAuthTokenUseCase implements IValidateAuthTokenUseCase {
+  constructor(private readonly jwt: JwtService) {}
+
+  async execute<T>({ token }: IValidateAuthTokenDTO) {
+    return (await this.jwt.verifyAsync(token)) as T;
+  }
+}
