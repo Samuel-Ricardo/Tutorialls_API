353f793822323decebbd069a265832361a9ea2b7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-26 21:57:09 -0300

    [ :card_file_box: ] | update: repository - user (module::application)

--- tutorialls/src/domain/repository/user/user.repository.ts
+++ tutorialls/src/domain/repository/user/user.repository.ts
@@ -5,6 +5,6 @@ import { User } from 'src/domain/entity/user.entity';
 
 export interface IUserRepository {
   signup(user: ISignupUserDTO): Promise<User>;
-  findByEmail(DTO: IFindUserByEmailDTO): Promise<User>;
-  findById(DTO: IFindUserByIdDTO): Promise<User>;
+  findByEmail(DTO: IFindUserByEmailDTO): Promise<User | undefined>;
+  findById(DTO: IFindUserByIdDTO): Promise<User | undefined>;
 }
