e71d1d0fb9a2d8debdc4516f291867073f16f250
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 16:18:47 -0300

    [ :safety_vest: ] | apply: pipe - validation > zod [user] (module::application)

--- tutorialls/src/application/users/users.controller.ts
+++ tutorialls/src/application/users/users.controller.ts
@@ -3,6 +3,9 @@ import { MODULE } from 'src/app.registry';
 import { ILoginUserDTO } from 'src/domain/DTO/user/login.dto';
 import { ISignupUserDTO } from 'src/domain/DTO/user/register.dto';
 import { IUserService } from 'src/domain/service/user/user.service';
+import { ZodValidationPipe } from './pipe/validation/zod/zod.pipe';
+import { SignupSchema } from './validation/zod/user/signup.schema';
+import { LoginSchema } from './validation/zod/user/login.schema';
 
 @Controller('user')
 export class UsersController {
@@ -12,12 +15,14 @@ export class UsersController {
   ) {}
 
   @Post('signup')
-  async signup(@Body() user: ISignupUserDTO) {
+  async signup(
+    @Body(new ZodValidationPipe(SignupSchema)) user: ISignupUserDTO,
+  ) {
     await this.service.signup(user);
   }
 
   @Post('login')
-  async login(@Body() user: ILoginUserDTO) {
+  async login(@Body(new ZodValidationPipe(LoginSchema)) user: ILoginUserDTO) {
     return await this.service.login(user);
   }
 }
