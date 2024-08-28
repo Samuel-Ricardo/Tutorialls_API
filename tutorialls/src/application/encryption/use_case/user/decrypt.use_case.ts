25f9876edf3a9039f4816a858c7baa65ce8121b3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-27 22:39:46 -0300

    [ :gear: ] | update: module - encryption [config] (module::infra)

--- tutorialls/src/application/encryption/use_case/user/decrypt.use_case.ts
+++ tutorialls/src/application/encryption/use_case/user/decrypt.use_case.ts
@@ -2,14 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { createDecipheriv } from 'crypto';
 import { IDecryptUserDTO } from 'src/domain/DTO/security/user/decrypt.dto';
 import { IDecryptUserUseCase } from 'src/domain/use_case/user/security/decrypt.use_case';
+import { EnvService } from 'src/infra/config/env/env.service';
 
 @Injectable()
 export class NodeDecryptUserUseCase implements IDecryptUserUseCase {
-  private readonly algorithm = 'aes-256-ctr';
-  private readonly secretKey = 'l20l8oiXQxRtFb7Ga6WwRNl9oMJKTiUA';
+  private readonly algorithm: string;
+  private readonly secretKey: string;
+
+  constructor(private readonly env: EnvService) {
+    this.secretKey = this.env.getEncryptKey();
+    this.algorithm = this.env.getEncryptAlgorithm();
+  }
 
   async execute({ ciphertext }: IDecryptUserDTO) {
-    const [iv, encryptedText] = ciphertext.split(':');
+    const [iv, encryptedText] = ciphertext.split(
+      this.env.getCipherBreakpoint(),
+    );
     const decipher = createDecipheriv(
       this.algorithm,
       this.secretKey,
