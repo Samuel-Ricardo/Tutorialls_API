a0f9ac1c2873565bc004a1aa24c835066559dee6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-29 19:16:32 -0300

    [ :sparkles: ] | implements: use-case - update > tutorial (module::domain)

--- tutorialls/src/application/tutorial/use_case/create.use_case.ts
+++ tutorialls/src/application/tutorial/use_case/create.use_case.ts
@@ -2,9 +2,10 @@ import { Inject, Injectable } from '@nestjs/common';
 import { MODULE } from 'src/app.registry';
 import { ICreateTutorialDTO } from 'src/domain/DTO/tutorial/create.dto';
 import { ITutorialRepository } from 'src/domain/repository/tutorial/tutorial.repository';
+import { ICreateTutorialUseCase } from 'src/domain/use_case/tutorials/create.use_case';
 
 @Injectable()
-export class CreateTutorialUseCase {
+export class CreateTutorialUseCase implements ICreateTutorialUseCase {
   constructor(
     @Inject(MODULE.TUTORIAL.REPOSITORY.PRISMA)
     private readonly tutorialRepository: ITutorialRepository,
--- tutorialls/src/application/tutorial/use_case/delete.use_case.ts
+++ tutorialls/src/application/tutorial/use_case/delete.use_case.ts
@@ -2,9 +2,10 @@ import { Inject, Injectable } from '@nestjs/common';
 import { MODULE } from 'src/app.registry';
 import { IDeleteTutorialDTO } from 'src/domain/DTO/tutorial/delete.dto';
 import { ITutorialRepository } from 'src/domain/repository/tutorial/tutorial.repository';
+import { IDeleteTutorialUseCase } from 'src/domain/use_case/tutorials/delete.use_case';
 
 @Injectable()
-export class DeleteTutorialUseCase {
+export class DeleteTutorialUseCase implements IDeleteTutorialUseCase {
   constructor(
     @Inject(MODULE.TUTORIAL.REPOSITORY.PRISMA)
     private readonly tutorialRepository: ITutorialRepository,
