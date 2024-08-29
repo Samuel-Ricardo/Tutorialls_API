010705bd61774c2256c0b110240087b68a6ee991
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-29 16:09:39 -0300

    [ :label: ] | create: DTO - pagination (module::domain)

---
+++ tutorialls/src/domain/DTO/pagination.dto.ts
@@ -0,0 +1,12 @@
+// pagination-query.dto.ts
+import { IsOptional, IsPositive } from 'class-validator';
+
+export class PaginationDTO {
+  @IsOptional()
+  @IsPositive()
+  page: number;
+
+  @IsOptional()
+  @IsPositive()
+  limit: number;
+}
