d0860c1028aa85c9c323017b22e4df56937aa342
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-28 23:37:49 -0300

    [ :adhesive_bandage: ] | fix: cors

--- tutorialls/src/main.ts
+++ tutorialls/src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new ExceptionFilter(httpAdapter));
