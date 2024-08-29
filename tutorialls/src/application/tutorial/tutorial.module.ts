b2818e695bd4f2d99798e345caceb5914aff7323
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-29 19:40:16 -0300

    [ :syringe: ] | update: module - tutorials (module::application)

--- tutorialls/src/application/tutorial/tutorial.module.ts
+++ tutorialls/src/application/tutorial/tutorial.module.ts
@@ -4,15 +4,55 @@ import { TutorialController } from './tutorial.controller';
 import { MODULE } from 'src/app.registry';
 import { PrismaTutorialRepository } from './repository/prisma/tutorial.repositiry';
 import { PrismaModule } from 'src/infra/engine/database/prisma/prisma.module';
+import { CreateTutorialUseCase } from './use_case/create.use_case';
+import { UpdateTutorialUseCase } from './use_case/update.use_case';
+import { DeleteTutorialUseCase } from './use_case/delete.use_case';
+import { FilterTutorialsByAuthorUseCase } from './use_case/filter/by/author.use_case';
+import { FilterTutorialsByTitleUseCase } from './use_case/filter/by/title.use_case';
+import { ListAllTutotialsUseCase } from './use_case/list/all.use_case';
+import { FilterTutorialsByKeywordUseCase } from './use_case/filter/by/keyword.use_case';
 
 @Module({
-  controllers: [TutorialController, PrismaModule],
+  imports: [PrismaModule],
+  controllers: [TutorialController],
   providers: [
     { provide: MODULE.TUTORIAL.SERVICE.MAIN, useClass: TutorialService },
     {
       provide: MODULE.TUTORIAL.REPOSITORY.PRISMA,
       useClass: PrismaTutorialRepository,
     },
+    {
+      provide: MODULE.TUTORIAL.USE_CASE.CREATE,
+      useClass: CreateTutorialUseCase,
+    },
+    {
+      provide: MODULE.TUTORIAL.USE_CASE.UPDATE,
+      useClass: UpdateTutorialUseCase,
+    },
+    {
+      provide: MODULE.TUTORIAL.USE_CASE.DELETE,
+      useClass: DeleteTutorialUseCase,
+    },
+    {
+      provide: MODULE.TUTORIAL.USE_CASE.LIST.ALL,
+      useClass: ListAllTutotialsUseCase,
+    },
+    {
+      provide: MODULE.TUTORIAL.USE_CASE.FILTER.BY.AUTHOR,
+      useClass: FilterTutorialsByAuthorUseCase,
+    },
+    {
+      provide: MODULE.TUTORIAL.USE_CASE.FILTER.BY.TITLE,
+      useClass: FilterTutorialsByTitleUseCase,
+    },
+
+    {
+      provide: MODULE.TUTORIAL.USE_CASE.FILTER.BY.KEYWORD,
+      useClass: FilterTutorialsByKeywordUseCase,
+    },
+  ],
+  exports: [
+    { provide: MODULE.TUTORIAL.SERVICE.MAIN, useClass: TutorialService },
   ],
 })
 export class TutorialModule {}
