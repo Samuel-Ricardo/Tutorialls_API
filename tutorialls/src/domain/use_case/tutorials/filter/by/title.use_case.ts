386293105ea02fc1680f8bfb2df1c53556073068
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-08-29 19:23:57 -0300

    [ :sparkles: ] | implements: use-case - filter > tutorial (module::domain)

--- tutorialls/src/domain/use_case/tutorials/filter/by/title.use_case.ts
+++ tutorialls/src/domain/use_case/tutorials/filter/by/title.use_case.ts
@@ -1,8 +1,9 @@
 import { IPaginationOutputDTO } from 'src/domain/DTO/pagination/output.dto';
+import { IFilterTutorialsByTitleDTO } from 'src/domain/DTO/tutorial/filter/by/title.dto';
 import { Tutorial } from 'src/domain/entity/tutorial.entity';
 
 export interface IFilterTutorialsByTitleUseCase {
   execute(
-    DTO: IFilterTutorialsByTitleUseCase,
+    DTO: IFilterTutorialsByTitleDTO,
   ): Promise<IPaginationOutputDTO<Tutorial>>;
 }
