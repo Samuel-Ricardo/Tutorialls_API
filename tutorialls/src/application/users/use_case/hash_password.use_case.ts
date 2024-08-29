ec435087f88c09e46c31d01644e2bfd9a140a53a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2024-08-29 11:20:16 -0300

    .

--- tutorialls/src/application/users/use_case/hash_password.use_case.ts
+++ tutorialls/src/application/users/use_case/hash_password.use_case.ts
@@ -7,6 +7,6 @@ import { bcrypt } from 'src/infra/engine/hashing/bcrypt.engine';
 @Injectable()
 export class BcryptHashPasswordUseCase implements IHashPasswordUseCase {
   execute({ password }: IHashPasswordDTO): Promise<string> {
-    return bcrypt.hash(password, EnvService.ENV.HASH.ROUNDS as number);
+    return bcrypt.hash(password, Number(EnvService.ENV.HASH.ROUNDS));
   }
 }
