ec3711d729c3c23ebda4c2f5c853400f9d3f8cbe
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2024-08-29 00:15:25 -0300

    [ 🩹 ] | fix: cors

--- tutorialls/src/main.ts
+++ tutorialls/src/main.ts
@@ -7,7 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors();
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new ExceptionFilter(httpAdapter));
